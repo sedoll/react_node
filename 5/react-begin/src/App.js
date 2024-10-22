7a8faef509d9d16fed2ac53c1c6bee4ecd32ab54
sedoll <do11anm@naver.com>
2024-10-22 20:12:18 +0900

    241022 리액트 공부

--- 5/react-begin/src/App.js
+++ 5/react-begin/src/App.js
@@ -1,7 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div>
-      <h1>Welcome back!</h1>
+    <div className="App">
+      <h1 className={styles.title}>Wellcome!</h1>
+      <Button text={'안녕하'}/>
+      <Button text={1234}/>
     </div>
   );
 }
---
+++ 5/react-begin/src/Button.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import styles from './Button.module.css'
+
+function Button({text}) {
+    return (
+        <div>
+            <button id={styles.btn}>{text}</button>
+            <button className={styles.btn}>{text}</button>
+        </div>
+    )
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired
+}
+
+export default Button;
